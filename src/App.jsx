b750e80026e253e51f9a876448c745946585899d
Guarize <pedroguarize@hotmail.com>
2021-12-26 11:17:58 -0300

    [style] - initial responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import MyToolbox from './components/MyToolbox';
 import HomeSection from './components/HomeSection';
 import PortfolioProvider from './provider/PortfolioProvider';
-import './styles/App.css';
 import Projects from './components/Projects';
 import EmailFixed from './components/EmailFixed';
+import './styles/App.css';
 
 export default function App() {
   return (
