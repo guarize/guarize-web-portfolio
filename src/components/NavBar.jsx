ede974b22adfdfd1dffd8ea22387b260f24c18f0
Guarize <pedroguarize@hotmail.com>
2022-01-04 14:39:19 -0300

    [feat] - improves burger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { stack as Menu } from 'react-burger-menu';
 import { Link } from 'react-scroll';
 import useWindowDimensions from '../services/WindowDimensions';
 import burgerStyles from '../services/BurgerStyles';
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const { screenWidth } = useWindowDimensions();
 
   const navItems = () => (
     <section className="header-navbar">
       <section className="header-menu">
-        <Link to="toolbox" smooth duration={0} offset={-55}>Skills</Link>
-        <Link to="projects" smooth duration={0} offset={-55}>Projects</Link>
-        <Link to="footer" smooth duration={0}>Contacts</Link>
+        <Link
+          to="toolbox"
+          smooth
+          duration={0}
+          offset={-55}
+          onClick={() => setMenuOpen(false)}
+        >
+          Skills
+        </Link>
+        <Link
+          to="projects"
+          smooth
+          duration={0}
+          offset={-55}
+          onClick={() => setMenuOpen(false)}
+        >
+          Projects
+        </Link>
+        <Link
+          to="footer"
+          smooth
+          duration={0}
+          onClick={() => setMenuOpen(false)}
+        >
+          Contacts
+        </Link>
       </section>
     </section>
   );
 
   return screenWidth <= 1015 ? (
-    <Menu right styles={burgerStyles} disableAutoFocus>
+    <Menu
+      isOpen={menuOpen}
+      onOpen={() => setMenuOpen(true)}
+      onClose={() => setMenuOpen(false)}
+      right
+      styles={burgerStyles}
+      disableAutoFocus
+    >
       {navItems()}
     </Menu>
   ) : (
