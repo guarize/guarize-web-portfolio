f4455a690f02cf8235dd86e8991a4234508f1e23
Guarize <pedroguarize@hotmail.com>
2022-01-18 11:18:30 -0300

    [feat] - scroll indicator functional

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import '../styles/ScrollIndicator.css';
 
 export default function ScrollIndicator() {
   return (
-    <div className="mouse_scroll">
-      <div className="mouse">
-        <div className="wheel" />
+    <Link to="toolbox" smooth duration={0}>
+      <div className="mouse_scroll">
+        <div className="mouse">
+          <div className="wheel" />
+        </div>
+        <div>
+          <span className="m_scroll_arrows unu" />
+          <span className="m_scroll_arrows doi" />
+          <span className="m_scroll_arrows trei" />
+        </div>
       </div>
-      <div>
-        <span className="m_scroll_arrows unu" />
-        <span className="m_scroll_arrows doi" />
-        <span className="m_scroll_arrows trei" />
-      </div>
-    </div>
+    </Link>
   );
 }
